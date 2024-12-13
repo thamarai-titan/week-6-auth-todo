41daa679a75ad093ce96c59c9c1140a4f7f6b697
Thamarai <tmanalan710@gmail.com>
2024-12-13 09:57:40 +0530

    the sign-in was added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const cors = require("cors")
+const cors = require("cors");
+const JWT_SECRET = "passispass";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 let users = [];
@@ -14,49 +15,55 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/signup",function(req, res){
+app.post("/signup", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-        const username = req.body.username
-        const password = req.body.password
+  const user = users.find(u => u.username === username);
 
-        const user = users.find(u=>u.username === username)
+  if (user) {
+    res.send({
+      message: "username already exists",
+    });
+  } else {
+    users.push({
+      username: username,
+      password: password,
+    });
+    res.send({
+      message: "Its done",
+    });
+  }
+});
 
-        if(user){
-            res.send({
-                message:"username already exists"
-            })
-        }
-        else{
-            users.push({
-                username:username,
-                password:password
-            })
-            res.send({
-                message:"Its done"
-            })
-        }
-        
-})
+app.post("/signin", function (req, res) {
+  const username = req.body.username?.trim();
+  const password = req.body.password?.trim();
 
-app.post("/signin",function(req, res){
+  console.log("Users list:", users);
+  console.log("Attempted login with:", req.body);
 
-})
+  const user = users.find(
+      user => user.username === username && user.password === password
+  );
 
-app.get("/mytodo",function(req, res){
+  if (!user) {
+      console.log("Login failed: No matching user found");
+      return res.status(401).send({ message: "Invalid username or password" });
+  }
 
-})
+  const token = jwt.sign({ username: username }, JWT_SECRET);
+  res.send({ token, message: "Sign-in successful" });
+});
 
-app.post("/save",function(req, res){
 
-})
+app.get("/mytodo", function (req, res) {});
 
-app.post("/edit",function(req, res){
+app.post("/save", function (req, res) {});
 
-})
+app.post("/edit", function (req, res) {});
 
-app.post("/delete",function(req, res){
-
-})
+app.post("/delete", function (req, res) {});
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
--- public/script.js
+++ public/script.js
@@ -4,6 +4,9 @@ async function signup() {
 
   const response = await fetch('http://localhost:3000/signup',{
     method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
     body: JSON.stringify({
         username:username,
         password:password
@@ -12,5 +15,25 @@ async function signup() {
   const data = await response.json()
   console.log(data)
   document.getElementById("signup-mesg").innerHTML = data.message
-  
+
 }
+async function signin() {
+  const username = document.getElementById("username-signin").value
+  const password = document.getElementById("password-signin").value
+
+  const response = await fetch('http://localhost:3000/signin',{
+    method:"POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        username:username,
+        password:password
+    })
+  })
+  const data = await response.json()
+  console.log(data)
+  document.getElementById("signin-mesg").innerHTML = data.message
+
+}
+
